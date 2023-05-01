fdface221653fc4a4038c9704a68b87d153acc0d
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2023-05-02 01:34:33 +0700

    feat: Initial

---
+++ src/Modules/Customers/Inflow.Modules.Customers.Api/Extensions.cs
@@ -0,0 +1,21 @@
+using System.Runtime.CompilerServices;
+using Inflow.Modules.Customers.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: InternalsVisibleTo("Inflow.Bootstrapper")]
+namespace Inflow.Modules.Customers.Api;
+
+internal static class Extensions
+{
+    public static IServiceCollection AddCustomersModule(this IServiceCollection services)
+    {
+        services.AddCore();
+        return services;
+    }
+
+    public static IApplicationBuilder UseCustomerModule(this IApplicationBuilder app)
+    {
+        return app;
+    }
+}
