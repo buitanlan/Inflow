20442727841c942bbfea20dff11dff7e13ff01ad
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2023-05-04 16:59:54 +0700

    feat(): dynamic module loader

---
+++ src/Modules/Customers/Inflow.Modules.Customers.Api/CustomersModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using Inflow.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: InternalsVisibleTo("Inflow.Bootstrapper")]
+namespace Inflow.Modules.Customers.Api;
+
+internal class CustomersModule : IModule
+{
+    public string Name { get; } = "Customers";
+    public void Register(IServiceCollection services)
+    {
+        services.AddCors();
+    }
+
+    public void Use(IApplicationBuilder app)
+    {
+        throw new NotImplementedException();
+    }
+}
